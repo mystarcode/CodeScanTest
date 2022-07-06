4dbb85c99ea5b78e28c7d238604d12d2f7cd7858
mystarcode <108800645+mystarcode@users.noreply.github.com>
2022-07-06 06:34:38 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,6 +14,17 @@ namespace CodeScanTest
                 Console.WriteLine("Saying hello: " + i + " times.");
             }
             Console.ReadLine();
+            RunQeury(5, "empty");
+        }
+
+        static void RunQeury(int a,string value)
+        {
+            string queryTest = "select * from dbo.people where rowid=" + a;
+            Console.WriteLine(queryTest);
+            if(value.Equals("No"))
+            {
+                Console.WriteLine("Matched");
+            }
         }
     }
 }
