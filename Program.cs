426745c47c5551f34132262aa045889e0bad1010
Mujahid Ullah Shaik (Contractor) <mujahid.ullah.shaik1@merative.com>
2022-07-06 15:59:02 +0530

    Code Checkin

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeScanTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Saying hello: " + i + " times.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
